7c4b8c9e84aecffa927d3a1f0c20e2c359b5abde
Aniss Elarj <aniss.elarj92@gmail.com>
2020-04-15 22:37:37 +0200

    ExportGroupe dynamique avec selection

--- resources/views/export-groupes.blade.php
+++ resources/views/export-groupes.blade.php
@@ -5,20 +5,44 @@
 
 <title>Export Groupes </title>
 
-<div class="container">
+<div class="container" style="width: 50rem;" >
     <div class="card bg-light mt-3">
         <div class="card-header">
-          Exportation Excel
+          Export Groupe
         </div>
         <div class="card-body">
+            <div class="row">
+            <div class="form-group col-md-3">
             <form action="{{ url('export-groupes') }}" method="POST" enctype="multipart/form-data">
                 @csrf
-                Nom du fichier : <input type"text" value="ListeGroupes" name="nom_fichier">.xls
-                <br><br>
-                <input type="submit" class="btn btn-success" value="Exporter" name="export_groupe">
-            </form>
-        </div>
-    </div>
+                 <input type"text" class="form-control" value="ListeGroupes" name="nom_fichier">
 </div>
+    
+
+<br><br>
+<div class="form-group col-md-3">
+<select class="form-control" name="groupe">
+
+  @foreach($listegroupes as $listegroupe)
+
+  <option value= "{{ $listegroupe->id_groupe }}" > {{ $listegroupe->libelle_groupe }} </option>
+  @endforeach
+</select>
+</div>
+<br>
+
+<div class="form-group col-md-3">
+<select class="form-control" name="annee">
+
+  @foreach($listeannees as $listeannee)
+
+  <option value= "{{ $listeannee->annee }}" > {{ $listeannee->annee }} </option>
+  @endforeach
+</select>
+</div>
+</div>
+    <input type="submit" class="btn btn-info" value="Exporter" name="export_groupe">
+            </form>
+      
 
 @endsection
